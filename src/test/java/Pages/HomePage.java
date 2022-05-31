d4b93623f282058cf736da304ef2da46dc4eb4a2
Ricardo Nere <ricardonere@gmail.com>
2022-05-31 17:55:28 -0300

    Criacao Metodos HomePage

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -5,6 +5,30 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     WebDriver driver;
 
+    String emailCadastro= "//input[@id='email_create']";
+    String criarConta= "//body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/button[1]/span[1]";
+    String genero= "//input[@id='id_gender1']";
+    String nome= "//input[@id='customer_firstname']";
+    String sobrenome= "//input[@id='customer_lastname']";
+    String email= "//input[@id='email']";
+    String senha= "//input[@id='passwd']";
+    String diaNasc= "//select[@id='days']";
+    String mesNasc= "//select[@id='months']";
+    String anoNasc= "//select[@id='years']";
+    String primeiroNomeEndereco= "//input[@id='firstname']";
+    String sobrenomeEndereco= "//input[@id='lastname']";
+    String companhia= "//input[@id='company']";
+    String endereco= "//input[@id='address1']";
+    String complementoEndereco= "//input[@id='address2']";
+    String cidade= "//input[@id='city']";
+    String estado= "//select[@id='id_state']";
+    String cep= "//input[@id='postcode']";
+    String pais= "//select[@id='id_country']";
+    String telefone= "//input[@id='phone_mobile']";
+    String enderecoAlternativo= "//input[@id='alias']";
+
+    }
+
     //http://automationpractice.com
     //input[@id='search_query_top']  digitar item
     //header/div[3]/div[1]/div[1]/div[2]/form[1]/button[1]   clicar busca
@@ -14,6 +38,10 @@ public class HomePage {
     //tbody/tr[@id='product_4_16_0_0']/td[5]/div[1]/a[2]/span[1]/i[1]   alterar a quantidade
     //tbody/tr[@id='product_4_16_0_0']/td[7]/div[1]/a[1]/i[1]  remocao do item
     //body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/p[2]/a[1]/span[1]  ir para a tela de checkout
+
+
+
+    //a[contains(text(),'Sign in')]   sign in
     //input[@id='email_create']  email de cadastro
     //body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/button[1]/span[1]  cria conta
     //input[@id='id_gender1']  genero
@@ -25,7 +53,7 @@ public class HomePage {
     //select[@id='months']  mes
     //select[@id='years']  ano
     //input[@id='firstname']  primeiro nome endereco
-    //input[@id='lastname']   sobrenome email
+    //input[@id='lastname']   sobrenome endereco
     //input[@id='company'] companhia
     //input[@id='address1']  endereco
     //input[@id='address2'] endereco complemento
